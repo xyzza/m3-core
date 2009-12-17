61da1c307111946543fe79820a12c547f496d658
Александр Кварацхелия <akvarats@bars-open.ru>
2009-12-17 11:33:24 +0300

    Комментарий к пакету m3.core

--- src/m3/db/management/migrations.py
+++ src/m3/db/management/migrations.py
@@ -1,3 +1,4 @@
+from django.forms.formsets import ManagementForm
 ﻿#coding:utf-8
 
 # --------------------------------------------------
@@ -5,3 +6,19 @@
 # при выполнении обновлений прикладной и системный частей
 # приложений
 # --------------------------------------------------
+
+from django.core import management
+
+class MigrationManager:
+    '''
+    Класс, выполняющий проверку и дальнейшую миграцию базы данных
+    '''
+    def __init__(self):
+        pass
+    
+    def migrate(self, applist):
+        '''
+        Выполняет миграцию приложений, указанных в applist
+        '''
+        for app in applist:
+            management.call_command('migrate', app)
