9a1a191a727e2981764f22253cf5e641c85ee299
Вадим Сафиуллин <vsafiullin@bars-open.ru>
2010-01-14 10:30:51 +0300

    Базовый класс для создания отчетов.

--- src/m3/core/excel_reporting/report_gen.py
+++ src/m3/core/excel_reporting/report_gen.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 
 import subprocess as sub
+from django.conf import settings
+from django.utils import importlib
 import json
 import sys
 import os
 
+#============================== ПАРАМЕТРЫ ================================
+
+def __get_template_path():
+    ''' Ищем корневую папку проекта '''
+    mod = importlib.import_module(settings.SETTINGS_MODULE)
+    settings_abs_path = os.path.dirname(mod.__file__)
+    return settings_abs_path
+    
 JAR_FULL_PATH = os.path.join(os.path.dirname(__file__), 'report.jar')
+DEFAULT_REPORT_TEMPLATE_PATH = __get_template_path()
 
 #============================= ИСКЛЮЧЕНИЯ ================================
 
@@ -63,4 +74,35 @@ def make_report_from_json_string(json_str):
     result_out, result_err = process.communicate(input = json_str.encode(encoding_name))
     __check_process(encoding_name, process, result_err)
  
+class BaseReport:
+    ''' Базовый класс для создания отчетов '''
+    
+    # Определяет путь к файлу шаблона относительно папки шаблонов
+    template_name = ''
+    
+    # Определяет путь к файлу результата относительно папки результатов
+    result_name = ''
+    
+    def make_report(self):
+        ''' Запускает формирование отчета '''
+        obj = self.collect()
+        if not isinstance(obj, dict):
+            raise ReportGeneratorError(u"Собранные данные должны быть упакованы в словарь")
+        if len(self.template_name) == 0:
+            raise ReportGeneratorError(u"template_name должен быть переопределен")
+        if len(self.result_name) == 0:
+            raise ReportGeneratorError(u"result_name должен быть переопределен")
+        
+        # Создаем абсолютные пути
+        obj["TEMPLATE_FILE_PATH"] = os.path.join(DEFAULT_REPORT_TEMPLATE_PATH, self.template_name)
+        obj["OUTPUT_FILE_PATH"]   = os.path.join(DEFAULT_REPORT_TEMPLATE_PATH, self.result_name)
+        
+        make_report_from_object(obj)
+    
+    def collect(self):
+        ''' Функция отвечающая за формирование данных '''
+        raise NotImplemented(u"Функция сбора данных должна быть переопределена в классах потомках")
+    
+    
+    
 
