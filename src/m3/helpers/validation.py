99134362387c8f91332790cfbf35aedbd11e80f1
Александр Кварацхелия <devnull@localhost>
2010-01-20 16:01:36 +0300

    Добавлены хелперы для валидации значений в пользовательских, например, формах

---
+++ src/m3/helpers/validation.py
@@ -0,0 +1,101 @@
+#coding: utf-8
+'''
+Хелперы для валидации значений
+Created on 20.01.2010
+
+@author: akvarats
+'''
+
+class Validator:
+    '''
+    Класс, обеспечивающий выполнение валидации объектов
+    на корректность их состояния.
+    '''
+    def __init__(self):
+        self.rules = []
+    
+#------------------------------------------------------------------------------ 
+# методы добавления правил валидации
+#------------------------------------------------------------------------------ 
+    def addrule_notempty(self, value, fail_msg, on_success=None, on_fail=None):
+        '''
+        Добавляет правило валидации значения на непустоту
+        ''' 
+        rule = NotEmptyValidationRule()
+        rule.value = value
+        rule.fail_msg = fail_msg
+        rule.callback_on_success = on_success
+        rule.callback_on_fail = on_fail
+        self.rules.append(rule)
+#------------------------------------------------------------------------------
+# методы добавления правил валидации 
+#------------------------------------------------------------------------------ 
+    def check(self, raise_exception=True):
+        result = []
+        for rule in self.rules:
+            if not rule.check():
+                result.append(rule.fail_msg)
+                if rule.callback_on_fail:
+                    rule.callback_on_fail()
+            else:
+                if rule.callback_on_success:
+                    rule.callback_on_success()
+        if result and raise_exception:
+            e = ValidationFailed()
+            for message in result:
+                e.add_error_message(message)
+            raise e
+            
+        return result
+        
+class BaseValidationRule(object):
+    '''
+    Базовое правило валидации значений.
+    
+    Ничего по сути не делает
+    '''
+    def __init__(self):
+        self.value = ''
+        self.fail_msg = ''
+        self.callback_on_success = None
+        self.callback_on_failed = None
+    def check(self):
+        return True
+        
+class NotEmptyValidationRule(BaseValidationRule):
+    '''
+    Правило валидации значения на непустоту
+    '''
+    def check(self):
+        return True if self.value else False
+
+#===============================================================================
+# Исключительная ситуация 
+#===============================================================================
+class ValidationFailed(Exception):
+    '''
+    Исключение для ситуаций валидации пользовательских данных
+    '''
+    def __init__(self):
+        self.error_messages = [] # список ошибок валидации
+        
+    def add_error_message(self, message):
+        self.error_messages.append(message)
+        
+    def has_errors(self):
+        return self.error_messages
+    
+    def messages_as_string(self, line_preffix = '  - ', line_delimiter='\n'):
+        '''
+        Преобразует self.error_messages в одну строку для отдачи в html
+        представление
+        '''
+        result = ''
+        for message in self.error_messages:
+            result += line_preffix + message + line_delimiter
+        if result:
+            result = result[0:len(result)-len(line_delimiter)]
+        return result
+    
+    def messages_to_html(self):
+        return self.messages_as_string(line_preffix = '&nbsp;&nbsp;-&nbsp;', line_delimiter='<br>')
