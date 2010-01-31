5a18ccc9e6d88704f556a761d58ecdfabe64c5af
Александр Кварацхелия <devnull@localhost>
2010-01-31 07:14:01 +0300

    В m3/__init__.py добавлена версия платформы

--- src/m3/__init__.py
+++ src/m3/__init__.py
@@ -1,1 +1,11 @@
-﻿#coding:utf-8
+#coding:utf-8
+'''
+Платформа разработки приложений ERP типа на python и django
+
+https://dev.bars-open.ru/bg/projects/show/m3
+'''
+
+# номер версии в формате major_version.minor_version.revision
+VERSION = (0,0,0)
+# дата выпуска версии
+VERSION_DATE = (0, 0, 0)
