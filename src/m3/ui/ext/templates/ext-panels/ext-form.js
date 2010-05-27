2b00019e4dbfaae7c6034fd916ef463a88147936
Александр Кварацхелия <devnull@localhost>
2010-05-27 12:01:18 +0400

    В шаблон ext-form.js добавлена обработка возможности задания поля, на которое будет установлен фокус при показе формы на редактирование.

--- src/m3/ui/ext/templates/ext-panels/ext-form.js
+++ src/m3/ui/ext/templates/ext-panels/ext-form.js
@@ -1,25 +1,29 @@
-new Ext.FormPanel({
-	{% include 'base-ext-ui.js'%}
-	
-	{% if component.url %} ,url: '{{ component.url }}' {% endif %}
-	
-	{% if component.layout %} ,layout: '{{ component.layout }}' {% endif %}
-	{% if component.layout_config %} ,layoutConfig: '{{ component.t_render_layout_config|safe }}' {% endif %}
-	
-	{% if component.icon_cls %} ,iconCls: '{{ component.icon_cls }}' {% endif %}
-	{% if component.title %} 
-		,title: '{{ component.title }}' 
-		,header: true
-	{% else %}
-		,header: false
-	{% endif %}
-	{% if component.top_bar %} ,tbar: {{ component.t_render_top_bar|safe }} {% endif %}
-	{% if component.buttom_bar %} ,bbar: {{ component.t_render_buttom_bar|safe }} {% endif %}
-	{% if component.footer_bar %} ,fbar: {{ component.t_render_footer_bar|safe }} {% endif %}
-	
- 	,baseCls: 'x-plain'
-	{% if component.label_width  %} ,labelWidth: {{ component.label_width }} {% endif %}
-	{% if component.label_align  %} ,labelAlign: '{{ component.label_align }}' {% endif %}
-	{% if component.label_pad  %} ,labelPad: {{ component.label_pad }} {% endif %}
-    ,items: [{{ component.t_render_items|safe }}]
-})
+(function(){
+    var form_panel = new Ext.FormPanel({
+    	{% include 'base-ext-ui.js'%}
+    	
+    	{% if component.url %} ,url: '{{ component.url }}' {% endif %}
+    	
+    	{% if component.layout %} ,layout: '{{ component.layout }}' {% endif %}
+    	{% if component.layout_config %} ,layoutConfig: '{{ component.t_render_layout_config|safe }}' {% endif %}
+    	
+    	{% if component.icon_cls %} ,iconCls: '{{ component.icon_cls }}' {% endif %}
+    	{% if component.title %} 
+    		,title: '{{ component.title }}' 
+    		,header: true
+    	{% else %}
+    		,header: false
+    	{% endif %}
+    	{% if component.top_bar %} ,tbar: {{ component.t_render_top_bar|safe }} {% endif %}
+    	{% if component.buttom_bar %} ,bbar: {{ component.t_render_buttom_bar|safe }} {% endif %}
+    	{% if component.footer_bar %} ,fbar: {{ component.t_render_footer_bar|safe }} {% endif %}
+    	
+     	,baseCls: 'x-plain'
+    	{% if component.label_width  %} ,labelWidth: {{ component.label_width }} {% endif %}
+    	{% if component.label_align  %} ,labelAlign: '{{ component.label_align }}' {% endif %}
+    	{% if component.label_pad  %} ,labelPad: {{ component.label_pad }} {% endif %}
+        ,items: [{{ component.t_render_items|safe }}]
+    });
+    {% if component.focused_field %}Ext.getCmp('{{ component.focused_field.client_id}}').focus(false, 1000);{% endif %}
+    return form_panel;
+})()
