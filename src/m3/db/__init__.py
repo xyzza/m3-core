042f0bed23f4a91ccbbe2808bd2590db71025aa0
Александр Кварацхелия <devnull@localhost>
2010-05-31 14:13:42 +0400

    В m3.db добавлена функция safe_delete, которая удаляет объект модели из базы данных с учетом того, что на нее могут быть ссылки в БД.

--- src/m3/db/__init__.py
+++ src/m3/db/__init__.py
@@ -1,1 +1,20 @@
-﻿#coding:utf-8
+#coding:utf-8
+
+from django.db import models, connection, transaction, IntegrityError
+
+def safe_delete(model):
+    '''
+    Класс, выполняющий "безопастное" удаление записи из БД.
+    
+    В случае, если удаление не удалось по причине нарушения целостности,
+    то возвращается false. Иначе, true
+    '''
+    try:
+        cursor = connection.cursor()
+        sql = "DELETE FROM %s WHERE id = %s" % (model.__class__._meta.db_table, model.id)
+        cursor.execute(sql)
+        transaction.commit_unless_managed()
+    except IntegrityError:
+        return False
+    
+    return True
