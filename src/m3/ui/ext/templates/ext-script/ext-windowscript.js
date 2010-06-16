7a382fa5b4d143db28f1de29f6e799e4d495a7dd
Александр Кварацхелия <devnull@localhost>
2010-06-16 17:13:49 +0400

    В скрипте окна добавлена функция, которую можно прописывать в хендлер кнопки "Закрыть".

--- src/m3/ui/ext/templates/ext-script/ext-windowscript.js
+++ src/m3/ui/ext/templates/ext-script/ext-windowscript.js
@@ -3,7 +3,9 @@
     win.show();
     
     {{ renderer.window.render_globals }}
-      
+    
+    function closeWindow(){ Ext.getCmp('{{window.client_id}}').close(); }
+    
     return win;
 })()
 
