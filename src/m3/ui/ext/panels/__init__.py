932bcdbadf57b8c0340d1f0b770d6ebe06887a6d
Александр Кварацхелия <devnull@localhost>
2010-06-16 17:09:42 +0400

    В m3/ui/ext/panels.py добавлен импорт ExtGridCheckBoxSelModel

--- src/m3/ui/ext/panels/__init__.py
+++ src/m3/ui/ext/panels/__init__.py
@@ -2,4 +2,4 @@
 
 from m3.ui.ext.containers import ExtPanel, ExtForm
 
-from grids import ExtGrid, ExtGridColumn, ExtGridDateColumn, ExtGridNumberColumn, ExtObjectGrid
+from grids import ExtGrid, ExtGridColumn, ExtGridDateColumn, ExtGridNumberColumn, ExtObjectGrid, ExtGridCheckBoxSelModel
