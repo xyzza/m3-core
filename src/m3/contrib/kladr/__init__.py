df60aed32028e45b6131d6ddfb14050d3a23d8f0
Айдар Гарифуллин <garifullin@bars-open.ru>
2010-06-30 10:28:12 +0400

    КЛАДР. транзакции каждые 10000 записей. прямое чтение улиц из dbf

--- src/m3/contrib/kladr/__init__.py
+++ src/m3/contrib/kladr/__init__.py
@@ -5,13 +5,13 @@
 
 from fill_kladr import fill_kladr
 
-def import_kladr(region = None, dbf_path = ''):
+def import_kladr(region_only = None, dbf_path = ''):
     '''
     Импортирует кладр из папки dbf_path. Если не задавать dbf_path,
     то система возьмет путь m3/externals.
     
     В случае если необходимо загрузить данные только по одному региону,
     то необходимо в region передать строку с двумя символами региона.
-    Например, region = '16'
+    Например, region_only = '16'
     '''
-    fill_kladr(region, dbf_path)
+    fill_kladr(region_only, dbf_path)
