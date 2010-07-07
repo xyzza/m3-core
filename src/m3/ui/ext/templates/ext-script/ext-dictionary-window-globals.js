16e357e9c22f499f946ec10f340d5cbf099dffef
Николай Телепенин <telepenin@bars-open.ru>
2010-07-07 17:31:28 +0400

    m3.js переделан в сязи с рефакторингом грида

--- src/m3/ui/ext/templates/ext-script/ext-dictionary-window-globals.js
+++ src/m3/ui/ext/templates/ext-script/ext-dictionary-window-globals.js
@@ -114,15 +114,13 @@ var ajax = Ext.Ajax;
 	 * Перезагружает хранилище данных
 	 */
 	function refreshGridStore(){
-		var bbar = Ext.getCmp("{{ component.grid.client_id }}").getBottomToolbar();
-		if (bbar && bbar.isXType('paging')) {
-			bbar.doRefresh();
-		} else {
+
 			var search_field = Ext.getCmp("{{ component.search_text_grid.client_id }}");
 			if (search_field) {
+				console.log(search_field);
 				search_field.search();
 			}
-		}
+
 	}
 	
 	/**
