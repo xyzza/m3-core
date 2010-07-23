043eb1aaa8951084f43873735693abec89002e9d
Илья Киров <devnull@localhost>
2010-07-23 18:06:35 +0400

    временно отключен "правильный" рендер окна

--- src/m3/ui/ext/windows/window.py
+++ src/m3/ui/ext/windows/window.py
@@ -17,23 +17,23 @@ class ExtWindow(BaseExtWindow):
         '''Рендерит конфиг, если указан layout'''
         return '{%s}' % ','.join(['%s:"%s"' % (k, v) for k, v in self.layout_config.items()])
     
-    def render_base_config(self):
-        super(ExtWindow, self).render_base_config()
-        if self.layout_config:
-            self._put_config_value('layoutConfig', self.t_render_layout_config())
-    
-    # Данный код должен находится в базовом классе, но т.к. не ве шаблоны 
-    # переведены на новый рендеринг, остается пока в каждом 
-    def render(self):
-        self.pre_render()
-        
-        try:
-            self.render_base_config()
-
-        except UnicodeDecodeError:
-            raise Exception('Some attribute is not unicode')
-        except Exception as msg:
-            raise Exception(msg) 
-        
-        base_config = self._get_config_str()
-        return  'new Ext.Window({%s})' %  base_config
+#    def render_base_config(self):
+#        super(ExtWindow, self).render_base_config()
+#        if self.layout_config:
+#            self._put_config_value('layoutConfig', self.t_render_layout_config())
+#    
+#    # Данный код должен находится в базовом классе, но т.к. не ве шаблоны 
+#    # переведены на новый рендеринг, остается пока в каждом 
+#    def render(self):
+#        self.pre_render()
+#        
+#        try:
+#            self.render_base_config()
+#
+#        except UnicodeDecodeError:
+#            raise Exception('Some attribute is not unicode')
+#        except Exception as msg:
+#            raise Exception(msg) 
+#        
+#        base_config = self._get_config_str()
+#        return  'new Ext.Window({%s})' %  base_config
