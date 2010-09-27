ca18650823429dd7c3f88577c14a93c8e6565ba5
Айдар Гарифуллин <garifullin@bars-open.ru>
2010-09-27 10:12:15 +0400

    В ExtTextArea добавлена маска

--- src/m3/ui/ext/templates/ext-fields/ext-text-area.js
+++ src/m3/ui/ext/templates/ext-fields/ext-text-area.js
@@ -1,7 +1,8 @@
 new Ext.form.TextArea({
 	{% include 'base-ext-ui.js'%}
 	{% include 'base-ext-field-ui.js'%}
-
+	
+	{% if component.mask_re %} ,maskRe: {{component.t_render_mask_re}} {% endif %}
 	{% if component.t_render_listeners %}
 		{# Прописываются имеющиеся обработчики #}
 		,listeners:{
