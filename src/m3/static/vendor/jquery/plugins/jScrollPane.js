fdb5865a05bfe75974108363ac50ff68a3ff963c
Ильдар Капкаев <devnull@localhost>
2010-10-21 11:58:54 +0400

    В Jquery scroll plugin добавлена поддержка скроллинга с тач устройств Ipad, Iphone, Ipod, Android

--- src/m3/static/vendor/jquery/plugins/jScrollPane.js
+++ src/m3/static/vendor/jquery/plugins/jScrollPane.js
@@ -188,6 +188,41 @@ jQuery.fn.jScrollPane = function(settings)
 									return false;
 								})
 						);
+						
+				// Ipad, Iphone, Ipod and Android support
+				//(192-223)
+				if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i)) || (navigator.userAgent.match(/Android/i))) {
+				 		var isTouchScreen = 1;
+				 	}else{
+				 		var isTouchScreen = 0;
+				 	}
+
+
+				$container.bind('touchstart', function(e){
+					var cpos = dragPosition;
+					if(isTouchScreen){
+									e = e.originalEvent.touches[0];
+								}
+					var sY = e.pageY;
+					var sX = e.pageX;
+
+
+					$container.bind('touchmove',function(ev){
+						if(isTouchScreen){
+							ev.preventDefault();
+							ev = ev.originalEvent.touches[0];
+						}
+
+						var top = cpos-(ev.pageY-sY);
+						positionDrag(top);
+
+					});
+					$container.bind('touchend',function(ev){
+						$container.unbind('touchmove touchend');
+					});
+				});
+				
+				
 					var $upArrow = jQuery('>.jScrollArrowUp', $container);
 					var $downArrow = jQuery('>.jScrollArrowDown', $container);
 					if (settings.arrowSize) {
