d227822d499688447d39d1dd8ae7843fac224d8c
Вадим Сафиуллин <vsafiullin@bars-open.ru>
2010-11-17 11:27:23 +0300

    Изменения в форме просмотра логов.

--- src/m3/contrib/logview/forms.py
+++ src/m3/contrib/logview/forms.py
@@ -6,48 +6,31 @@ Created on 25.10.2010
 '''
 import datetime
 
-from m3.ui.ext.windows.base import BaseExtWindow
 from m3.ui.ext.controls.buttons import ExtButton
 from m3.ui.ext.shortcuts import js_close_window
-from m3.ui.ext.containers.forms import ExtFieldSet
 from m3.ui.ext.fields.simple import ExtComboBox, ExtTextArea, ExtDateField
-from m3.ui.ext.containers.containers import ExtContainer
+from m3.ui.ext.containers.container_complex import ExtContainerTable
+from m3.ui.ext.windows.window import ExtWindow
         
-class ExtLogsWindow(BaseExtWindow):
+class ExtLogsWindow(ExtWindow):
     def __init__(self, window_params=None, *args, **kwargs):
         super(ExtLogsWindow, self).__init__(*args, **kwargs)
         self.__dict__.update(window_params)
-        self.title=u'Система логирования'
-        self.width = 750
-        self.height = 500
+        self.title=u'Просмотр логов'
+        self.min_width, self.min_height = self.width, self.height = 750, 500
         self.modal = False
-        self.layout = 'border'
+        self.maximizable = True
+        self.layout = 'vbox'
+        self.layout_config = {'align': 'stretch'}
         self.template_globals = 'ui-js/mis-admin.js'
-        fs_period = ExtFieldSet(
-            title=u'Выберите период', 
-            label_width = 120,
-            height=70,
-            layout = 'hbox',
-            region = 'north'
-            )
-        #Так как при layout hbox текст label скрыт, обарачиваем элементы
-        date_container = ExtContainer(
-            layout='form', 
-            label_width = 30
-            )
-        logFilesCombo_container = ExtContainer(layout='form')
         
-        date = ExtDateField(
-            label=u'Дата', 
+        self.date = ExtDateField(
+            label=u'Выберите дату', 
             name='start_date',
             allow_blank=False, 
-            hide_today_btn=True
-            )
-        date.value = datetime.datetime.now()
-        date_container.items.append(date)
-        
-        self.date = date
-        
+            hide_today_btn=True,
+            value = datetime.datetime.now()
+        )        
         self.logFilesCombo  = ExtComboBox(
             label = u'Выберите файл',
             editable = False, 
@@ -56,11 +39,13 @@ class ExtLogsWindow(BaseExtWindow):
             mode = 'remote',
             trigger_action_all = 'true'
         )
-        logFilesCombo_container.items.append(self.logFilesCombo)
         
-        fs_period.items.extend([date_container, logFilesCombo_container])
+        self.text_field = ExtTextArea(region='center', read_only=True, flex=1)
         
-        self.text_field = ExtTextArea(region='center', read_only=True)
+        fs_period = ExtContainerTable(columns=3, rows=1, height=28)
+        fs_period.set_item(row=0, col=0, cmp=self.date, width=200)
+        fs_period.set_item(row=0, col=2, cmp=self.logFilesCombo, width=300)
+        fs_period.set_properties(col_num=1, width=30)
             
         self.items.extend([fs_period, self.text_field])
         
