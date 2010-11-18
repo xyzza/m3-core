4be90ea64fff1bfb3ed9e3909a46bbbda1e98755
Вадим Сафиуллин <vsafiullin@bars-open.ru>
2010-11-18 20:12:38 +0300

    В мониторинг запросов добавлен разрез по адресам. Проведен рефакторинг. ВНИМАНИЕ! Убран глобальный try..except. Если подключен мониторинг не забывайте сдалать миграцию.

--- src/m3/contrib/usermon/__init__.py
+++ src/m3/contrib/usermon/__init__.py
@@ -1,14 +1,30 @@
 #coding:utf-8
-#from django.http import HttpResponse
-from django.contrib.auth import get_user
-from datetime import datetime 
-from .models import UserActivity, RequestActivity
-'''
-Набор инструкций, предназначенных для механизма мониторинга активности пользователей
+"""
+Мониторинг активности пользователей и статистика времени обработки запросов
+"""
+from datetime import datetime
+from collections import defaultdict
 
-@author: telepenin
-date created: 16.02.10
-'''
+from models import UserActivity, RequestActivity
+
+class Stat:
+    """ Содержит статистические данные за период """
+    def __init__(self):
+        # количество запросов от аутентифицированных пользователей                      
+        self.auth_request_count = 0
+        # от анонимов
+        self.anon_request_count = 0
+        # суммарное время обработки запросов от аутентифицированных пользователей
+        self.auth_request_sum_time_load = 0
+        # от анонимов
+        self.anon_request_sum_time_load = 0
+        
+    def get_avg_request_time(self):
+        return round(self.auth_request_sum_time_load / self.auth_request_count) if self.auth_request_count else 0
+    
+    def get_a_avg_request_time(self):
+        return round(self.anon_request_sum_time_load / self.anon_request_count) if self.anon_request_count else 0
+
 
 class MonitoringController(object):
     '''
@@ -22,39 +38,17 @@ class MonitoringController(object):
     ## Класс используется с Python singleton design pattern
     # Необходимо добавить все переменные и методы внутрь интерфейса Singleton
     class Singleton:
+        # Период добавления информации о средних показателях, в секундах (по умолчанию 15*60 = 15 минут)
+        DT_REQ_ACTIVITY = 15*60
+        # Период добавления информации об активности пользователей, в секундах (по умолчанию 60 = 1 минута) 
+        DT_USER_ACTIVITY = 60 
         
         def __init__(self):
-            '''
-                auth_request_count - количество запросов от аутентифицированных пользователей
-                anon_request_count - от анонимов
-                
-                auth_request_sum_time_load - суммарное время обработки запросов от аутентифицированных пользователей
-                anon_request_sum_time_load - от анонимов
-                
-                first_request_time - время первого запроса, от которого отсчитывается интервал до сохранения
-                     информации о средних показателях
-                     
-               last_user_activity - определяется в _flush - время последней активности пользователя 
-                   (не меньше определенного значения)
-            '''
-            self.auth_request_count = 0
-            self.anon_request_count = 0
-            
-            self.auth_request_sum_time_load = 0
-            self.anon_request_sum_time_load = 0
-            
-            self.last_request_time = datetime.now()  
+            self.buffer = defaultdict(lambda: Stat())
+            # время первого запроса, от которого отсчитывается интервал до сохранения информации о средних показателях
+            self.last_request_time = datetime.now()
+            # время последней активности пользователя (не меньше определенного значения)
             self.first_request_time = datetime.now()
-            #self.last_user_activity = datetime.now()
-            
-        def request_end_processing(self, user):
-            '''
-            Сохраняет среднее значение
-            '''
-            if user.is_authenticated():
-                self.auth_request_sum_time_load += self._get_deltatime()
-            else:
-                self.anon_request_sum_time_load += self._get_deltatime()
 
         def _get_deltatime(self):
             '''
@@ -63,66 +57,58 @@ class MonitoringController(object):
             dt = datetime.now() - self.last_request_time
             return dt.seconds*10**3 + dt.microseconds*10**-3
         
-        def add_request_stat(self, user):
+        def add_request_stat(self, request):
             '''
             Фиксирует запрос в экземпляре
             '''
+            user = request.user
+            path = request.path
             self._flush(user) # может быть уже пора сделать сохранение в базу?
             if user.is_authenticated():
-                self.auth_request_count += 1
+                self.buffer[path].auth_request_count += 1
             else: 
-                self.anon_request_count += 1
+                self.buffer[path].anon_request_count += 1
             
             self.last_request_time = datetime.now()
+            
+        def request_end_processing(self, request):
+            '''
+            Сохраняет среднее значение
+            '''
+            user = request.user
+            path = request.path
+            if user.is_authenticated():
+                self.buffer[path].auth_request_sum_time_load += self._get_deltatime()
+            else:
+                self.buffer[path].anon_request_sum_time_load += self._get_deltatime()
         
         def _flush(self, user):
             '''
             Заносит информацию в бд и обнуляет единственный экземпляр класса
             '''
+            # Если пользователь авторизован, то надо записать время его активности
             if user.is_authenticated():
-                dt_user_activity = 60 # Период добавления информации об активности пользователей, в секундах (по умолчанию 60 = 1 минута)
-                if not hasattr(self, 'last_user_activity') or (datetime.now() - self.last_user_activity).seconds > dt_user_activity:
-                    # добавление записей в UserActivity 
-                    
-                    if UserActivity.objects.filter(user_id=user).count() == 1 :
-                        user_activity = UserActivity.objects.get(user_id=user)
-                    elif UserActivity.objects.filter(user_id=user).count() > 1:
-                        UserActivity.objects.filter(user_id=user).delete()
-                        user_activity = UserActivity()
-                    else:
-                        user_activity = UserActivity()
-                    
-                    user_activity.user_id = user
-                    user_activity.save()    
-                    
+                if not hasattr(self, 'last_user_activity') or \
+                   (datetime.now() - self.last_user_activity).seconds > self.DT_USER_ACTIVITY:
+
+                    UserActivity.objects.get_or_create(user_id=user.id, user_name=user.username) 
                     self.last_user_activity = datetime.now()       
             
-            dt_req_activity = 15*60 # Период добавления информации о средних показателях, в секундах (по умолчанию 15*60 = 15 минут)
-            if self.first_request_time and (datetime.now() - self.first_request_time).seconds > dt_req_activity: # нужно записать информацию в б
-                # добавление в RequestActivity
-                req_activity = RequestActivity()
-                req_activity.period = datetime(datetime.now().year, 
-                                               datetime.now().month, 
-                                               datetime.now().day, 
-                                               datetime.now().hour,
-                                               datetime.now().minute,
-                                               datetime.now().second,)
-                # Аутентифицированные:
-                req_activity.total_requests = self.auth_request_count
-                if self.auth_request_count:
-                    req_activity.avg_request_time = round(self.auth_request_sum_time_load / self.auth_request_count)
-                else:
-                    req_activity.avg_request_time = 0
+            # Записываем статистику по времени отработки запросов
+            if self.first_request_time and (datetime.now() - self.first_request_time).seconds > self.DT_REQ_ACTIVITY:
                 
-                # Анонимы:
-                req_activity.a_total_requests = self.anon_request_count
-                if self.anon_request_count:
-                    req_activity.a_avg_request_time = round(self.anon_request_sum_time_load / self.anon_request_count)
-                else:
-                    req_activity.a_avg_request_time = 0
-                
-                #req_activity.request_type = None # Пока не трогаем это поле
-                req_activity.save()
+                now = datetime.now()
+                period = datetime(now.year, now.month, now.day, now.hour, now.minute, now.second)
+                for url, stat in self.buffer.items():
+                    RequestActivity.objects.create(
+                        url = url,
+                        period = period,
+                        total_requests = stat.auth_request_count,
+                        a_total_requests = stat.anon_request_count,
+                        # Средние показатели
+                        avg_request_time = stat.get_avg_request_time(),
+                        a_avg_request_time = stat.get_a_avg_request_time()
+                    )
                 
                 # очищаем текущий экземпляр, достаточно заново проинициализировать
                 self.__init__()
@@ -132,9 +118,6 @@ class MonitoringController(object):
         if MonitoringController._instance is None:
             # Создание экземпляра, если не существует
             MonitoringController._instance = MonitoringController.Singleton()
- 
-        # Ссылка на единственный экземпляр 
-        # self._EventHandler_instance = MonitoringController._instance
  
     # Передает доступ к реализации синглтона
     def __getattr__(self, aAttr):
@@ -151,21 +134,12 @@ class UsermonMiddleware:
     '''
     
     def process_request(self, request):
-        try:
-            mon = MonitoringController()
-            mon.add_request_stat(get_user(request))
-        except:
-            pass    
-#        import time # тесты
-#        time.sleep(1)
-        return None
+        mon = MonitoringController()
+        mon.add_request_stat(request)
     
     def process_response(self, request, response): 
-        try:
-            mon = MonitoringController()
-            mon.request_end_processing(get_user(request))
-        except:
-            pass
+        mon = MonitoringController()
+        mon.request_end_processing(request)
         
 ##     тесты
 #        if mon.auth_request_count:
