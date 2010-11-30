3f5f8cb853fbc99cdca214e2ca1b637f17b702b9
Ильдар Капкаев <devnull@localhost>
2010-11-30 10:52:39 +0300

    Добавлена поддержка touch скроллина(Ipad, Iphone, Ipod and Android)

--- src/m3/static/vendor/jquery/plugins/jquery.jscrollpane.js
+++ src/m3/static/vendor/jquery/plugins/jquery.jscrollpane.js
@@ -271,6 +271,40 @@
 						}
 
 						appendArrows(verticalTrack, settings.verticalArrowPositions, arrowUp, arrowDown);
+                        
+                        // Ipad, Iphone, Ipod and Android support(275-308)
+                        // Добавлена поддержка touch скроллина, сторонее дополнение написанное народнымим умельцами 
+                        if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i)) || (navigator.userAgent.match(/Android/i))) {
+                                var isTouchScreen = 1;
+                            }else{
+                                var isTouchScreen = 0;
+                            }
+        
+        
+                        container.bind('touchstart', function(e){
+                            var cpos = dragPosition;
+                            if(isTouchScreen){
+                                            e = e.originalEvent.touches[0];
+                                        }
+                            var sY = e.pageY;
+                            var sX = e.pageX;
+        
+        
+                            container.bind('touchmove',function(ev){
+                                if(isTouchScreen){
+                                    ev.preventDefault();
+                                    ev = ev.originalEvent.touches[0];
+                                }
+        
+                                var top = cpos-(ev.pageY-sY);
+                                positionDrag(top);
+        
+                            });
+                            container.bind('touchend',function(ev){
+                                container.unbind('touchmove touchend');
+                            });
+                        });
+                        
 					}
 
 					verticalTrackHeight = paneHeight;
