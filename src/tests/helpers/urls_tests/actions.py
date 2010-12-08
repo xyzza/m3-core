b11b0d093b378656806e595c51fe8405fd2c9295
Александр Кварацхелия <devnull@localhost>
2010-12-08 19:31:20 +0300

    в механизм reversed urls добавлена возможность работы с короткими именами экшенов (см. тесты)

--- src/tests/helpers/urls_tests/actions.py
+++ src/tests/helpers/urls_tests/actions.py
@@ -12,6 +12,7 @@ class TestActionPack1(actions.ActionPack):
     '''
     url = '/pack1'
     
+    
     def __init__(self):
         super(TestActionPack1, self).__init__()
         self.actions.extend([TestAction11(), 
@@ -21,10 +22,11 @@ class TestActionPack1(actions.ActionPack):
 class TestActionPack2(actions.ActionPack):
     '''
     '''
-    url = '/pack2'    
+    url = '/pack2'
     
     def __init__(self):
         super(TestActionPack2, self).__init__()
+        self.shortname = 'test-action-12'
         self.actions.extend([TestAction21, 
                              TestAction22(),
                              'tests.helpers.urls_tests.actions.TestAction23',])
@@ -34,27 +36,38 @@ class TestAction11(actions.Action):
     '''
     '''
     url = '/action1'
+    shortname = 'test-action-11'
+    
     
 class TestAction12(actions.Action):
     '''
     '''
     url = '/action2'
+    short_name = 'test-action-12'
     
 class TestAction13(actions.Action):
     '''
     '''
     url = '/action3'
+     
 
 
 class TestAction21(actions.Action):
     '''
     '''
     url = '/action1'
+    def __init__(self):
+        super(TestAction21, self).__init__()
+        self.shortname = 'test-action-21'
     
 class TestAction22(actions.Action):
     '''
     '''
     url = '/action2'
+    
+    def __init__(self):
+        super(TestAction22, self).__init__()
+        self.shortname = 'test-action-22'
     
 class TestAction23(actions.Action):
     '''
