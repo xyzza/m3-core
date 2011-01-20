dec1d1a66a1a9feb68a1cb62f1a1af7c12ad34ee
Ильдар Капкаев <devnull@localhost>
2011-01-20 17:09:53 +0300

    RE: Заглушка для IE6

--- src/m3/contrib/m3_checkie/actions.py
+++ src/m3/contrib/m3_checkie/actions.py
@@ -7,13 +7,14 @@ Created on 20.01.2011
 from django.shortcuts import render_to_response
 from m3.ui.actions import Action, ActionPack
 
-class Check_IE6(Action):
+class CheckIE6(Action):
     '''
     Заглушка для IE6
+    Возвращает страничку с информацией, и предложением скачать актуальные браузеры
     '''
     url = '/ie6'
     def run(self, request, context):
-        return render_to_response('index.html')
+        return render_to_response('check_ie6.html')
         
 
 class CheckBrowserActionsPack(ActionPack):
@@ -22,7 +23,5 @@ class CheckBrowserActionsPack(ActionPack):
     '''
     def __init__(self):
         super(self.__class__,self).__init__()
-        self.Check_IE6 = Check_IE6()
-        
-        self.actions = [ self.Check_IE6]
+        self.actions.append(CheckIE6())
         
