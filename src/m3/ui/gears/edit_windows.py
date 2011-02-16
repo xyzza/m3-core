ddfc13ca2596dbe6ba6c7339145aa8b482532035
Ахмадиев Тимур <devnull@localhost>
2011-02-16 11:29:07 +0300

    Перенесли из миса окно GearEditWindow

--- src/m3/ui/gears/edit_windows.py
+++ src/m3/ui/gears/edit_windows.py
@@ -11,25 +11,48 @@ from m3.ui.ext import windows
 from m3.ui.ext import panels
 from m3.ui.ext import controls
 from m3.ui.ext import containers
-
+from m3.ui.actions import Action
 
 class GearEditWindow(windows.ExtEditWindow):
     '''
     Окно редактирования, в котором лежит форма и в котором есть кнопки OK и Отмена.
-    
     Размеры окна по умолчанию 600x400 
     ''' 
     def __init__(self, *args, **kwargs):
         super(GearEditWindow, self).__init__(*args, **kwargs)
         
-        self.width = 600
-        self.height = 400
-        self.title = ''
+        self.frozen_size(600, 400)
+        self.title = u'Не забудь написать заголовок'
         
         self.form = panels.ExtForm()
         
-        self.buttons.extend([controls.ExtButton(text=u'ОК', handler='submitForm'),
-                             controls.ExtButton(text=u'Отмена', handler='cancelForm')])
+        self.btn_save = controls.ExtButton(
+            text = u'Сохранить', 
+            handler = 'submitForm'
+        )
+        self.btn_cancel = controls.ExtButton(
+            name = 'cancel_btn', 
+            text = u'Отмена', 
+            handler = 'cancelForm'
+        )
+        
+        self.buttons.extend([self.btn_save, self.btn_cancel])
+        
+        self._submit_action = None
+    
+    def frozen_size(self, width, height):
+        """ Устанавливает размер и заодно делает его минимально допустимым """
+        self.width, self.height = width, height
+        self.min_width, self.min_height = width, height
+    
+    def _set_submit_action(self, value):
+        """ Из переданного типа экшена пытается получить адрес для формы """
+        if isinstance(value, Action):
+            self.form.url = value.get_absolute_url()
+        elif issubclass(value, Action):
+            self.form.url = value.absolute_url()
+
+    submit_action = property(lambda: self._submit_action, _set_submit_action)
         
         
 class GearTableEditWindow(GearEditWindow):
