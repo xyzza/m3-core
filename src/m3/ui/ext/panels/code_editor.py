9c4f58045f9157278b1bcb9201b2cbe5382c48e0
Даниил Ганиев <devnull@localhost>
2011-03-10 18:13:01 +0300

    Добавил контрол редактора кода

---
+++ src/m3/ui/ext/panels/code_editor.py
@@ -0,0 +1,28 @@
+#coding: utf-8
+from m3.ui.ext.containers.forms import ExtPanel
+
+__author__ = 'daniil-ganiev'
+
+class ExtCodeEditor(ExtPanel):
+    '''Редактор кода в браузере'''
+
+    def __init__(self, *args, **kwargs):
+        super(ExtCodeEditor, self).__init__(*args, **kwargs)
+        self.source_code = '#Put some code here'
+        self.read_only = False
+
+        self.init_component(*args, **kwargs)
+
+    def render_params(self):
+        super(ExtCodeEditor, self).render_params()
+        self._put_params_value('sourceCode', self.source_code)
+        self._put_params_value('readOnly', self.read_only)
+
+    def render(self):
+        self.render_base_config()
+        self.render_params()
+        base_config = self._get_config_str()
+        params_config = self._get_params_str()
+        return 'new Ext.ux.panel.CodeEditor({%s}, {%s})' % (base_config, 
+                                                            params_config)
+  
