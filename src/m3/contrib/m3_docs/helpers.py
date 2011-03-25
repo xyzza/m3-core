f5fba28f21c7cbdf100684196f856c372ad3ab6c
Zinoviev Igor <devnull@localhost>
2011-03-25 15:31:23 +0300

    m3_docs: добавил построение документа с учетом вложенности секций

--- src/m3/contrib/m3_docs/helpers.py
+++ src/m3/contrib/m3_docs/helpers.py
@@ -28,13 +28,24 @@ class DesignerConfigAdapter:
         }
 
         all_fields = list(DocumentField.objects.filter(document_type = doc_type))
-        all_section = list(DocumentSection.objects.filter(document_type = doc_type))
+        all_sections = list(DocumentSection.objects.filter(document_type = doc_type))
 
-        #добавим все поля в корень
-
+        #код дальше отражает мысль: "Я наконец нашел куда засунуть list comprehensions"
         result['items'].extend(
             [ self._get_document_field_data(field) for field in all_fields if field.section is None ]
         )
+
+        def prepare_section(section, fields, sections):
+            new_section = self._get_document_section_data(section)
+            new_section['items'].extend(
+                [self._get_document_field_data(field) for field in fields if field.section == section])
+            new_section['items'].extend(
+                [prepare_section(s, fields, sections) for s in sections if s.parent == section]
+            )
+            return new_section
+
+        result['items'].extend([ prepare_section(section, all_fields, all_sections) for section in all_sections
+                                if section.parent is None])
 
         return json.dumps(result)
 
