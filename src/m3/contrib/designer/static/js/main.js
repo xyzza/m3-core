93abe4553c198036bbc1ca1cabc1fdb73b211c04
Николай Телепенин <telepenin@bars-open.ru>
2011-04-12 16:46:30 +0400

    Дизайнер: Мелкие переименования. Выводится наименование проекта в качестве название корневого узла.

--- src/m3/contrib/designer/static/js/main.js
+++ src/m3/contrib/designer/static/js/main.js
@@ -1,92 +1,99 @@
 /**
  * 
  */
-
-var tree = new Ext.tree.TreePanel({
-	useArrows: true
-    ,autoScroll: true
-    ,animate: true    
-    ,containerScroll: true
-    ,border: false					        
-    ,loader: new Ext.tree.TreeLoader({
-		url: '/project-files'
-	})					
-    ,root: {
-        nodeType: 'async'
-        ,text: 'Файлы проекта'
-        ,draggable: false
-        ,id: 'source'
-        ,expanded: true					     
-    }
-	,contextMenu: new Ext.menu.Menu({
-	        items: [{
-	            id: 'create-file'
-	            ,text: 'Создать файл'
-	            ,iconCls: 'icon-script-add'
-	        },{
-	            id: 'rename-file'
-	            ,text: 'Переименовать файл'
-	            ,iconCls: 'icon-script-edit'
-	        },{
-	            id: 'delete-file'
-	            ,text: 'Удалить файл'
-	            ,iconCls: 'icon-script-delete'
-	        },'-',{
-	        	id: 'create-class'
-	            ,text: 'Добавить класс'
-	            ,iconCls: 'icon-cog-add'
-	            ,handler: function(item, e){
-			
-					Ext.MessageBox.prompt('Создание класса', 
-						'Введите название класса',
-						function(btn, text){
-							if (btn == 'ok'){
-								var node = item.parentMenu.contextNode;
-					            var attr = node.attributes;
-				            	Ext.Ajax.request({
-				            		url:'/create-new-class'
-				            		,params: {
-				            			path: attr['path']
-				            			,className: text
-				            		}
-				            		,success: function(){
-				            			var new_node = new Ext.tree.TreeNode({
-				            				text: text
-				            				,path: attr['path']
-				            				,class_name: text
-				            				,iconCls: 'icon-page-white-c'	
-				            				,leaf: true			            				
-				            			});
-
-				            			node.appendChild(new_node);
-				            		},
-				            		failure: uiAjaxFailMessage
-				            	});
-				            }
-						}						
-					);
-	            }
-	        }
-	        ]
-	    }),
-	    listeners: {
-	        contextmenu: function(node, e) {
-	            node.select();	            
-	            if (node.text === 'ui.py' || node.text === 'forms.py' ) {
-		            var c = node.getOwnerTree().contextMenu;
-		            c.contextNode = node;
-		            c.showAt(e.getXY());						            	
-	            }
-
-	        },
-	        dblclick: function(node, e){	        	
-	        	if (node.parentNode && (node.parentNode.text === 'ui.py' || node.parentNode.text === 'forms.py' ) ){
-		        	onClickNode(node);
-	        	}
-	        }
-	    }	
-})
-
+function createTreeView(rootNodeName){
+	var tree =  new Ext.tree.TreePanel({
+		useArrows: true
+	    ,autoScroll: true
+	    ,animate: true    
+	    ,containerScroll: true
+	    ,border: false					        
+	    ,loader: new Ext.tree.TreeLoader({
+			url: '/project-files'
+		})					
+	    ,root: {
+	        nodeType: 'async'
+	        ,text: rootNodeName
+	        ,draggable: false
+	        ,id: 'source'
+	        ,expanded: true					     
+	    }
+		,contextMenu: new Ext.menu.Menu({
+		        items: [{
+		            id: 'create-file'
+		            ,text: 'Создать файл'
+		            ,iconCls: 'icon-script-add'
+		        },{
+		            id: 'rename-file'
+		            ,text: 'Переименовать файл'
+		            ,iconCls: 'icon-script-edit'
+		        },{
+		            id: 'delete-file'
+		            ,text: 'Удалить файл'
+		            ,iconCls: 'icon-script-delete'
+		        },'-',{
+		        	id: 'create-class'
+		            ,text: 'Добавить класс'
+		            ,iconCls: 'icon-cog-add'
+		            ,handler: function(item, e){
+				
+						Ext.MessageBox.prompt('Создание класса', 
+							'Введите название класса',
+							function(btn, text){
+								if (btn == 'ok'){
+									var node = item.parentMenu.contextNode;
+						            var attr = node.attributes;
+					            	Ext.Ajax.request({
+					            		url:'/create-new-class'
+					            		,params: {
+					            			path: attr['path']
+					            			,className: text
+					            		}
+					            		,success: function(){
+					            			var new_node = new Ext.tree.TreeNode({
+					            				text: text
+					            				,path: attr['path']
+					            				,class_name: text
+					            				,iconCls: 'icon-page-white-c'	
+					            				,leaf: true			            				
+					            			});
+	
+					            			node.appendChild(new_node);
+					            		},
+					            		failure: uiAjaxFailMessage
+					            	});
+					            }
+							}						
+						);
+		            }
+		        }
+		        ]
+		    }),
+		    listeners: {
+		        contextmenu: function(node, e) {
+		            node.select();	            
+		            if (node.text === 'ui.py' || node.text === 'forms.py' ) {
+			            var c = node.getOwnerTree().contextMenu;
+			            c.contextNode = node;
+			            c.showAt(e.getXY());						            	
+		            }
+	
+		        },
+		        dblclick: function(node, e){	        	
+		        	if (node.parentNode && (node.parentNode.text === 'ui.py' || node.parentNode.text === 'forms.py' ) ){
+			        	onClickNode(node);
+		        	}
+		        }
+		    }	
+	})
+	
+	
+	tree.getLoader().on("beforeload", function(treeLoader, node) {	
+    	treeLoader.baseParams['path'] = node.attributes.path;
+	}, this);
+	
+	return tree;
+}
 
 var tabPanel = new Ext.TabPanel({	
 	region: 'center'
@@ -118,6 +125,3 @@ function onClickNode(node) {
 }
 
 
-tree.getLoader().on("beforeload", function(treeLoader, node) {	
-    treeLoader.baseParams['path'] = node.attributes.path;
-}, this);
