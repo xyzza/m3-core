46b0ddd78e0319c90e21e784a93719c5e29c6089
Гусев Данил <devnull@localhost>
2011-04-29 11:36:54 +0400

    Исправление безмолного импорта app_meta

--- src/m3/helpers/urls.py
+++ src/m3/helpers/urls.py
@@ -29,18 +29,20 @@ def get_app_urlpatterns():
     вручную в urls.py прикладных приложений
     '''
     url_patterns = urls.defaults.patterns('',)
-    
+
     for app_name in settings.INSTALLED_APPS:
         try:
             module = importlib.import_module('.app_meta', app_name)
-        except ImportError:
+        except ImportError, err:
             # по идее, такая ошибка возникает в случае, если у нас для установленного приложения
             # нет модуля app_meta.py
+            if err.args[0].find('No module named') == -1:
+                raise
             continue
         proc = getattr(module, 'register_urlpatterns', None)
         if callable(proc):
             url_patterns += proc()
-            
+
     return url_patterns
 
 def get_pack(pack_name):
@@ -64,15 +66,15 @@ def get_action(action_name):
     '''
     action_data = ActionsNameCache().get(action_name, None)
     return action_data[0] if action_data else None
-    
-    
+
+
 def get_url(action_name):
     '''
     Возвращает абсолютный путь до 
     '''
     action_data = ActionsNameCache().get(action_name, None)
     return action_data[1] if action_data else ''
-    
+
 get_acton_url = get_url
 
 def get_pack_url(pack_name):
@@ -90,15 +92,15 @@ def get_pack_by_url(url):
     packs = collections.deque([])
     for controller in ControllerCache.get_controllers():
         packs.extend(controller.top_level_packs)
-        
+
     while len(packs) > 0:
         pack = packs.popleft()
         if hasattr(pack, 'subpacks'):
             packs.extend(pack.subpacks)
-        
+
         cleaned_pack = get_instance(pack)
         if url == cleaned_pack.__class__.absolute_url():
-            return cleaned_pack  
+            return cleaned_pack
     return None
 #===============================================================================
 # Кеш, используемый для хранения соответствия экшенов
@@ -108,13 +110,13 @@ class ActionsNameCache(caching.IntegralRuntimeCache):
     Кеш, используемый для хранения соответствия имен экшенов и паков 
     соответствующим пакам
     '''
-    
+
     def handler(self, cache, dimentions):
         '''
         Хендлер сборки кеша
         '''
         try:
-            return inner_name_cache_handler(True) 
+            return inner_name_cache_handler(True)
         except:
             logger.exception(u'Cannot run handler of ActionsNameCache.')
 
@@ -125,18 +127,18 @@ class PacksNameCache(caching.IntegralRuntimeCache):
     Кеш, используемый для хранения соответствия имен экшенов и паков 
     соответствующим пакам
     '''
-    
+
     def handler(self, cache, dimentions):
         '''
         Хендлер сборки кеша
         '''
         try:
-            return inner_name_cache_handler(False) 
+            return inner_name_cache_handler(False)
         except:
             logger.exception(u'Cannot run handler of ActionsNameCache.')
 
         return {}
-    
+
 def inner_name_cache_handler(for_actions=True):
     '''
     Внутренний метод обхода дерева паков и экшенов.
@@ -149,7 +151,7 @@ def inner_name_cache_handler(for_actions=True):
         Сам объект экшена или пака передается в параметре
         obj.
         '''
-        names = ['shortname', 'short_name',]
+        names = ['shortname', 'short_name', ]
         objects = [obj.__class__, obj]
         for o in objects:
             for name in names:
@@ -157,22 +159,22 @@ def inner_name_cache_handler(for_actions=True):
                     return getattr(o, name, '')
 
         return ''
-         
+
     # TODO посмотреть как работает для врапнутых классов
     result = {}
-    
+
     ControllerCache.populate()
     # что-то внутренность данного метода не вызывает
     # доверия, если честно
-    
+
     packs = collections.deque([])
-    
+
     controllers = actions.ControllerCache.get_controllers()
-    
+
     # считываем паки верхнего уровня
     for controller in controllers:
         packs.extend(controller.top_level_packs)
-        
+
     while len(packs) > 0:
         pack = packs.popleft()
         # субпаки - в очередь!
@@ -180,14 +182,14 @@ def inner_name_cache_handler(for_actions=True):
             packs.extend(pack.subpacks)
         if for_actions and hasattr(pack, 'actions'):
             for action in pack.actions:
-                
+
                 # неважно что нам передали, нам нужен экземпляр класса
                 cleaned_action = get_instance(action)
-                
+
                 key = cleaned_action.__class__.__module__ + '.' + cleaned_action.__class__.__name__
                 url = cleaned_action.__class__.absolute_url()
                 result[key] = (cleaned_action.__class__, url, cleaned_action)
-                
+
                 shortname = get_shortname(cleaned_action)
                 if shortname:
                     result[shortname] = (cleaned_action.__class__, url, cleaned_action)
@@ -199,5 +201,5 @@ def inner_name_cache_handler(for_actions=True):
             shortname = get_shortname(cleaned_pack)
             if shortname:
                 result[shortname] = (cleaned_pack.__class__, url, cleaned_pack)
-            
+
     return result
