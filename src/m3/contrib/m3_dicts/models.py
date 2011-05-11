fd222872ac064db7a24f4ce2f10f076109801f28
Ольга Кузьмина <devnull@localhost>
2011-05-11 11:09:08 +0400

    В m3_dicts добавлен справочник "Документы, удостоверяющие личность"

--- src/m3/contrib/m3_dicts/models.py
+++ src/m3/contrib/m3_dicts/models.py
@@ -1,1 +1,16 @@
 #coding:utf-8
+
+from django.db import models
+
+from m3.db import BaseObjectModel
+
+
+class DulType(BaseObjectModel):
+    '''
+    Модель документов, удостоверяющих личность
+    '''
+    code = models.CharField(max_length=20, null=True, blank=True)
+    name = models.CharField(max_length=200)
+    
+    class Meta:
+        db_table = 'm3_dicts_dultype'
