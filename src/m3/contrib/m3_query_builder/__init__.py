78a0c2a1758f3f72c3287e837d8a2b33dd10906a
Николай Телепенин <telepenin@bars-open.ru>
2011-05-26 18:24:29 +0400

    Заготовка по контруктору запросов

---
+++ src/m3/contrib/m3_query_builder/__init__.py
@@ -0,0 +1,56 @@
+#coding: utf-8
+'''
+Created on 25.05.2011
+
+@author: prefer
+'''
+
+import threading
+import inspect
+
+from django.conf import settings
+from django.utils.importlib import import_module
+
+from entity import BaseEntity
+
+class EntityCache(object):
+    '''
+    Загружает и хранит список 
+    '''
+    
+    _loaded = False
+    _write_lock = threading.RLock()
+    _entities = []
+    
+    @classmethod
+    def populate(cls):
+        '''
+        Собирает в отдельном потоке все сущности из установленных приложений с 
+        названием schema
+        '''
+        if not cls._loaded:
+            with cls._write_lock:
+                for app_name in settings.INSTALLED_APPS:
+                    try:
+                        module = import_module('.schema', app_name)
+                    except ImportError, err:
+                        if 'No module named' not in err.args[0]:
+                            raise
+                        continue
+                                        
+                    cls._entities = [obj for _, obj in inspect.getmembers(module)
+                                        if inspect.isclass(obj)
+                                            and hasattr(obj, '__module__') 
+                                            and obj.__module__ == module.__name__
+                                            and issubclass(obj, BaseEntity)]
+
+                cls._loaded = True
+                
+                
+    @classmethod        
+    def get_entities(cls):
+        '''
+        Возвращает список
+        '''
+        cls.populate()        
+        return cls._entities
---
+++ src/m3/contrib/m3_query_builder/entity.py
@@ -0,0 +1,63 @@
+#coding: utf-8
+'''
+Created on 26.05.2011
+
+@author: prefer
+'''
+from m3.helpers.datastructures import TypedList
+
+class Relation(object):
+    '''
+    Связь между сущностями
+    '''
+    def __init__(self, entity_first, key_first, entity_second, key_second):
+        '''
+        @param table_first: Первая сущность
+        @param key_first: Первый ключ для связи
+        @param entity_second: Вторая сущность
+        @param key_second: Второй ключ для связи 
+        '''
+        self.table_first = entity_first
+        self.key_first = key_first
+        
+        self.table_second = entity_second
+        self.key_second = key_second
+        
+        
+class BaseEntity(object):
+    '''
+    Базовый класс для сущностей/схемы/прокси/view - кому как удобно
+    В дальнейшем будем употреблять "сущность"
+    ps: view в контексте бд
+    '''
+    
+    # Имя сущности
+    name = None
+    
+    # Список объектов (модели, сущности, имена таблиц), используемых во текущей
+    # сущности. Если нужны алиасы, то их необходимо использовать здесь в виде: 
+    # entities = [{'m1':'MyModel1'}, {'m2':'MyModel2'}, MyModel1'] 
+    # m1 - алиас модели MyModel1
+    entities = []
+    
+    # Типизированный список связей, над вышеупомянутами объектами
+    relations = TypedList(type=Relation)
+    
+    # Список полей, по которым нужно проводить группировку
+    group_by = []
+    
+    # Объект условий
+    where = None
+    
+    # Словарь с алиасами для полей в select'e запроса
+    select = {}
+    
+    # Выводить повторяющиеся записи?
+    distinct = None
+    
+    # Количество показываемых записей
+    limit = None
+
+
+        
+        
