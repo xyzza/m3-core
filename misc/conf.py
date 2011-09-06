24df04225c8ffc33dad0e74ccebdd079088a0d93
Даниил Ганиев <devnull@localhost>
2011-09-06 09:56:38 +0400

    Переструктуризация яваскрипт файлов №4

--- misc/conf.py
+++ misc/conf.py
@@ -15,23 +15,25 @@ OUTER_JS_FOLDER = os.path.join(STATIC_PATH, 'ext', 'js')
 # Путь до своих js
 INNER_JS_FOLDER = os.path.join(STATIC_PATH, 'm3', 'js')
 
+# Путь до своих расширений экста
+OUR_EXTJS_EXTENSIONS_FOLDER = os.path.join(INNER_JS_FOLDER, 'ext-extensions')
+
+# Путь до своих прочих js файлов
+OUR_OTHER_JS_FOLDER = os.path.join(INNER_JS_FOLDER, 'other')
+
 # Какие типы файлов
-FILE_EXTENSIONS = ('js',)
+FILE_EXTENSIONS = ('js', )
 
 # Внешние приоритетные js файлы для загрузки
 HIGH_PRIORITY_OUTER = ('Reorderer.js', )
 
 # Приоритетные js файлы для загрузки
-HIGH_PRIORITY = ('m3.js', 
-                 'ComboBox.js', 
-                 'Grid.js', 
-                 'TreeGrid.js',
-                 'Window.js', 
-                 'AdvancedTreeGrid.js',
-                 )
-# Файлы с наименьшем приоритетом
+HIGH_PRIORITY = ('m3.js', 'ComboBox.js', 'Grid.js', 'TreeGrid.js', 'Window.js',
+    'AdvancedTreeGrid.js',)
+
+# Файлы с наименьшим приоритетом
 LOW_PRIORITY = ('FileUploadField.js', 'ImageUploadField.js', 
-                'containers.js', 'override.js',)
+    'containers.js', 'override.js',)
 
 # Название файла
 FILE_NAME = 'm3-debug.js'
@@ -40,5 +42,6 @@ FILE_NAME = 'm3-debug.js'
 FILE_NAME_OPT = 'm3-opt.js'
 
 #Игнорируемые файлы
-EXCLUDE = ('calendar-all.js', 'calendar-all-debug.js','Calendar.js', FILE_NAME, FILE_NAME_OPT, 'MultiGroupingGrid.js')
+EXCLUDE = ('calendar-all.js', 'calendar-all-debug.js','Calendar.js',
+           FILE_NAME, FILE_NAME_OPT, 'MultiGroupingGrid.js')
 
