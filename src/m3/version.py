84944ff8a1c67438809450188693d7766cdb22b3
Александр Кварацхелия <devnull@localhost>
2011-09-12 15:51:42 +0400

    version.py для платформы версии 1.0

--- src/m3/version.py
+++ src/m3/version.py
@@ -3,4 +3,11 @@
 __version__ = '1.0'
 __appname__ = u'Платформа М3'
 
-__require__ = {}
+__require__ = {
+    'django': '1.3',
+    'south': '0.7.3',
+    'mptt': '0.4.2',
+    'sqlalchemy': '0.7.1',
+    'dbfpy': '2.2.5',
+    'xlwt': '0.7.2',
+}
