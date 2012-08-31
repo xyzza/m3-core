96f7ad9f43e3062c6d05070e6803147a2d427936
Вахотин Игорь <vahotin@bars-open.ru>
2012-08-31 09:26:29 +0400

    Исправлен баг возникающий при отображении ExtDictSelectField и ExtMultiSelectField

--- src/m3/static/m3/js/ext-extensions/AdvancedCombobox.js
+++ src/m3/static/m3/js/ext-extensions/AdvancedCombobox.js
@@ -191,13 +191,13 @@ Ext.m3.AdvancedComboBox = Ext.extend(Ext.m3.ComboBox, {
 			
             var triggerIndex = 'Trigger'+(index+1);
             t.hide = function(){
-                var w = triggerField.wrap.getWidth();
+                var w = triggerField.getWidth();
                 this.dom.style.display = 'none';
                 triggerField.el.setWidth(w-triggerField.trigger.getWidth());
                 this['hidden' + triggerIndex] = true;
             };
             t.show = function(){
-                var w = triggerField.wrap.getWidth();
+                var w = triggerField.getWidth();
                 this.dom.style.display = '';
                 triggerField.el.setWidth(w-triggerField.trigger.getWidth());
                 this['hidden' + triggerIndex] = false;
