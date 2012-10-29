8af3ed2667faaced0f35bb9dcb2acd78a924263c
Илья Киров <devnull@localhost>
2012-10-29 14:44:25 +0400

    Фикс к предыдущему

--- src/m3/static/m3/js/ext-extensions/MultiGroupingGrid.js
+++ src/m3/static/m3/js/ext-extensions/MultiGroupingGrid.js
@@ -1354,9 +1354,9 @@ Ext.m3.MultiGroupingGridPanel = Ext.extend(Ext.ux.grid.livegrid.GridPanel, {
                     this.getBottomToolbar()];
         for (var i=0; i<toolbars.length; i++){
             if (toolbars[i]){
-                var itemState = state.pop();
+                var itemState = state.shift();
                 for (var j=0; j<toolbars[i].items.length; j++){
-                    var disabled = itemState.pop();
+                    var disabled = itemState.shift();
                     toolbars[i].items.items[j].setDisabled(disabled);
                 }
             }
