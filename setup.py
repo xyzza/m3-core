bb2b72834bec3b6c419af781bfa6600dabaca8c1
Telepenin Nikolay <telepenin@bars-open.ru>
2013-05-24 16:38:47 +0400

    Название пакета изменено на m3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requires = []
 with open('src/requires.txt', 'r') as f:
     requires.extend(f.readlines())
 
-setup(name='m3-ext4',
+setup(name='m3',
       version='1.5.1.1',
       url='https://src.bars-open.ru/py/m3/m3',
       license='Apache License, Version 2.0',
