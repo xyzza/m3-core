78fcc2d23602160e02ea99d83931a1a14f3f1a4a
Пирогов Алексей <devnull@localhost>
2013-06-24 15:28:19 +0400

    permission checker теперь может быть указан строкой + версия 1.0.6.16

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('src/requires.txt', 'r') as f:
     requires.extend(f.readlines())
 
 setup(name='m3',
-      version='1.0.6.15',
+      version='1.0.6.16',
       url='https://src.bars-open.ru/py/m3/m3',
       license='Apache License, Version 2.0',
       author='BARS Group',
