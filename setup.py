9b685f0330f97c517c668bce07c206e6cc82cc0e
dmatveev <dmatveev@BO-Matveev>
2013-07-10 17:01:47 +0400

    Версия 1.5.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('src/requires.txt', 'r') as f:
     requires.extend(f.readlines())
 
 setup(name='m3',
-      version='1.5.2',
+      version='1.5.3',
       url='https://src.bars-open.ru/py/m3/m3',
       license='Apache License, Version 2.0',
       author='BARS Group',
