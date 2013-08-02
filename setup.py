44b7e72cd829d8865a9196f61f094c6f7f7d108a
A. Torsunov <torsunov@bars-open.ru>
2013-08-02 14:49:16 +0400

    v 1.5.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('src/requires.txt', 'r') as f:
     requires.extend(f.readlines())
 
 setup(name='m3',
-      version='1.5.5',
+      version='1.5.6',
       url='https://src.bars-open.ru/py/m3/m3',
       license='Apache License, Version 2.0',
       author='BARS Group',
