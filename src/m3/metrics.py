52715093be70b13a7a35141f2162e0895bf5b2d2
Борисов Кирилл <borisov@bars-open.ru>
2013-12-09 00:17:32 +0400

    Функция отправки контекстов на удаленный сервер.

--- src/m3/metrics.py
+++ src/m3/metrics.py
@@ -1,10 +1,18 @@
 #coding: utf-8
 
+import json
+import hashlib
+import logging
+import time
+import urllib2
+
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.sessions.models import Session
 from django.db.models.signals import post_delete, post_save
 from django.contrib.auth.signals import user_logged_in, user_logged_out
+
+from m3.actions import ControllerCache
 
 try:
     import pystatsd
@@ -15,6 +23,7 @@ except ImportError as ie:
 prefix = getattr(settings, 'METRICS_PREFIX', None)
 host = getattr(settings, 'METRICS_HOST', 'localhost')
 port = getattr(settings, 'METRICS_PORT', 8125)
+endpoint_url = getattr(settings, 'METRICS_CONTEXTS_URL', 'http://localhost:1942/register')
 
 statsd_client = pystatsd.Client(host, port, prefix=prefix)
 
@@ -50,4 +59,38 @@ def capture_users_metrics(statsd_client):
 
         User.__metrics_registered = True
 
-capture_users_metrics(statsd_client)
+def send_controllers_contexts():
+    def get_hash(s):
+        h = hashlib.md5()
+        h.update(s)
+        return h.hexdigest()
+
+    ControllerCache.populate()
+
+    controllers = [{
+                    get_hash(c.url): {
+                        'url': c.url
+                    }
+                } for c in ControllerCache._controllers]
+
+    parts = prefix.split('.')
+    identity = dict(zip(['version', 'region', 'client', 'product'], parts))
+    instance_id = filter(lambda x: x.startswith('instance_'), parts)
+    if instance_id:
+        identity['instance'] = instance_id[0][9:]
+
+    packet = {
+        'timestamp': int(time.time()),
+        'info': {
+            'controller': controllers,
+        },
+        'identity': identity
+    }
+
+    try:
+        req = urllib2.Request(endpoint_url, 
+                              data=json.dumps(packet),
+                              headers={'Content-Type': 'application/json'})
+        resp = urllib2.urlopen(req)
+    except urllib2.URLError as ue:
+        logging.error("Can't send contexts to {0}: {1}".format(endpoint_url, ue))
